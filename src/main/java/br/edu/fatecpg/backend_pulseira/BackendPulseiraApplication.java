3fa927657803417fe02b07e17a302d5aaa5ec7ba
João Pedro <joaopedroliveira98@gmail.com>
2026-09-08 15:47:20 -0300

    first commit

---
+++ src/main/java/br/edu/fatecpg/backend_pulseira/BackendPulseiraApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fatecpg.backend_pulseira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPulseiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPulseiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/fatecpg/backend_pulseira/BackendPulseiraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fatecpg.backend_pulseira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPulseiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
